e1d721afbf668fa9585266d8977e0ff5b1435c60
ragesh-pakkath <70128119+ragesh-pakkath@users.noreply.github.com>
2020-09-14 23:53:34 +0530

    Update build.sh

--- app2/build.sh
+++ app2/build.sh
@@ -1,3 +1,3 @@
-docker build . -t webapp1:latest
-docker tag webapp1:latest localhost:5000/webapp1:latest
-docker push localhost:5000/webapp1:latest
+docker build . -t webapp2:latest
+docker tag webapp1:latest localhost:5000/webapp2:latest
+docker push localhost:5000/webapp2:latest
